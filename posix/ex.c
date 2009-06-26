ef3bb7d7ac8915f7948fa80c91997d1e4813af36
medgar123 <medgar123@92337893-2631-0410-a617-21b8a9ae7266>
2009-06-26 15:13:23 +0000

    Fix __close method for pipes due to change in Lua 5.1.3 fixing this bug: http://www.lua.org/bugs.html#5.1.2-1
    git-svn-id: http://lua-ex-api.googlecode.com/svn/trunk@6 92337893-2631-0410-a617-21b8a9ae7266

--- posix/ex.c
+++ posix/ex.c
@@ -483,9 +483,9 @@ int luaopen_ex(lua_State *L)
   if (lua_isnil(L, -1)) return luaL_error(L, "io not loaded");
   copyfields(L, ex_iolib, ex, -1);
   lua_getfield(L, ex, "pipe");                /* . io ex_pipe */
-  lua_getfield(L, -2, "stderr");              /* . io ex_pipe io_stderr */
-  lua_getfenv(L, -1);                         /* . io ex_pipe io_stderr E */
-  lua_setfenv(L, -3);                         /* . io ex_pipe io_stderr */
+  lua_getfield(L, -2, "open");                /* . io ex_pipe io_open */
+  lua_getfenv(L, -1);                         /* . io ex_pipe io_open E */
+  lua_setfenv(L, -3);                         /* . io ex_pipe io_open */
   /* extend the io.file metatable */
   luaL_getmetatable(L, LUA_FILEHANDLE);       /* . F */
   if (lua_isnil(L, -1)) return luaL_error(L, "can't find FILE* metatable");
